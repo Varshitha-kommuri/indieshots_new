e8af1754bc6b030e0046e38a7952fede9a2dae53
Sasi_Vardhan_Vemuri <163669536+Sasi-Vardhan@users.noreply.github.com>
2025-07-27 22:56:42 +0530

    Update main.tsx

--- indieshots-07-25/client/src/main.tsx
+++ indieshots-07-25/client/src/main.tsx
@@ -2,6 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "next-themes";
+import { app } from './firebaseConfig';
+
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider 
